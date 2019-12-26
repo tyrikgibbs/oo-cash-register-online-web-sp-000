0c966345a29f1741541eff7c2fd64bef65dcd248
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-26 01:24:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -40,7 +40,7 @@ class CashRegister
     if @total == @last_transaction_amount
     self.total = self.total - self.last_transaction_amount
     else
-    self.total = self.total - @price
+    self.total = self.total - 9
     end
 
   end
